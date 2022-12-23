e1a6118132eae21fb14b6a2bffa913db80425190
Valionas <38500314+Valionas@users.noreply.github.com>
2022-12-23 13:01:58 +0200

    Sring Generated Start Project

---
+++ farmManager/src/test/java/com/farmManager/farmManager/FarmManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmManager.farmManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
