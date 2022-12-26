91c846c6d7b49ef8b8685cac69972b0fa51759c0
Valionas <38500314+Valionas@users.noreply.github.com>
2022-12-26 14:26:50 +0200

    List All Animals Page

--- farmManager-web/src/app/app.component.ts
+++ farmManager-web/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Animal } from './models/animal';
+import { AnimalService } from './services/animal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'farmManager-web';
+export class AppComponent implements OnInit {
+  public animals: Animal[] | undefined;
+  public editAnimal: Animal | undefined;
+  public deleteAnimal: Animal | undefined;
+  constructor(private animalService: AnimalService) { }
+
+  ngOnInit(): void {
+    this.getAnimals();
+  }
+
+  public getAnimals(): void {
+    this.animalService.getAnimals().subscribe(
+      (response: Animal[]) => {
+        this.animals = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
 }
---
+++ farmManager-web/src/app/models/animal.ts
@@ -0,0 +1,9 @@
+export interface Animal {
+    id: number;
+    name: string;
+    category: string;
+    animalType: string;
+    foodType: string;
+    age: number;
+    imageUrl: string;
+}
