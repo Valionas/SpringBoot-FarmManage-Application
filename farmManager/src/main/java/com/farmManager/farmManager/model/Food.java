0e29b0804c75feb4f67d110967616747eebfb8b5
Valionas <38500314+Valionas@users.noreply.github.com>
2023-01-01 12:12:39 +0200

    Food and Navigation Update

--- farmManager/src/main/java/com/farmManager/farmManager/model/Food.java
+++ farmManager/src/main/java/com/farmManager/farmManager/model/Food.java
@@ -12,4 +12,28 @@ public class Food {
     private String name;
     private Double quantity;
     private String imageUrl;
+
+    public Food() {}
+
+    public Food(String name, Double quantity, String imageUrl){
+        this.name = name;
+        this.quantity = quantity;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() { return id;}
+
+    public void setId(Long id) { this.id = id;}
+
+    public String getName() { return name;}
+
+    public void setName(String name) { this.name = name;}
+
+    public Double getQuantity() { return quantity;}
+
+    public void setQuantity(Double quantity){ this.quantity = quantity;}
+
+    public String getImageUrl() { return imageUrl;}
+
+    public void setImageUrl(String imageUrl){ this.imageUrl = imageUrl;}
 }
